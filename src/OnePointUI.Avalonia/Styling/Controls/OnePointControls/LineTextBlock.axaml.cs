cde7d07cc9a0aaf148ea3ed0e391b71808027b71
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-09-13 00:19:50 +0800

    feat: 加入 LineTextBlock.axaml

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/LineTextBlock.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
+
+public class LineTextBlock : TemplatedControl
+{
+    public static readonly StyledProperty<string> TextProperty =  AvaloniaProperty.Register<LineTextBlock, string>(nameof(Text));
+    public string Text 
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
