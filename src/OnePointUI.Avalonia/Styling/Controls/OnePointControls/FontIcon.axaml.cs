f6da7a2399e2d6212a8233cf6e059a0ea2b4f4dc
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-09-19 19:00:20 +0800

    feat: add font icon

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/FontIcon.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
+
+public class FontIcon : TemplatedControl
+{
+    
+    public static readonly StyledProperty<string> GlyphProperty =
+        AvaloniaProperty.Register<FontIcon, string>(nameof(Glyph), "");
+
+    public string Glyph
+    {
+        get => GetValue(GlyphProperty);
+        set => SetValue(GlyphProperty, value);
+    }
+}
