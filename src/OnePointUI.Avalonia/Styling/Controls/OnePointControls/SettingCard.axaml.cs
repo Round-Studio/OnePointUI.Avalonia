d0c5691c1218fb785d8ffbd7a125bccf758eab69
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-09-24 21:57:47 +0800

    feat: 非非非非非常吊的动画 ovo

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/SettingCard.axaml.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
+
+public class SettingCard : Button
+{
+    public static readonly StyledProperty<string> GlyphProperty =
+        AvaloniaProperty.Register<SettingCard, string>(nameof(Glyph), "");
+    
+    public static readonly StyledProperty<string> HeaderProperty =
+        AvaloniaProperty.Register<SettingCard, string>(nameof(Header));
+    
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<SettingCard, string>(nameof(Description));
+    
+    public static readonly StyledProperty<bool> IsClickableProperty =
+        AvaloniaProperty.Register<SettingCard, bool>(nameof(IsClickable));
+
+    public string Glyph
+    {
+        get => GetValue(GlyphProperty);
+        set => SetValue(GlyphProperty, value);
+    }
+    public string Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+    public bool IsClickable
+    {
+        get => GetValue(IsClickableProperty);
+        set => SetValue(IsClickableProperty, value);
+    }
+}
