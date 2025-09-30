06fa61bbfc7e1b78038451c15e484d8d3984f2b8
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-09-30 23:35:37 +0800

    feat: 加载圈

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/LabelBox.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
+
+public class LabelBox : TemplatedControl
+{
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<LabelBox, string>(nameof(Text), string.Empty);
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/ProgressRing.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
+
+public class ProgressRing : TemplatedControl
+{
+}
