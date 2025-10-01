37b69fd2e2ae4bb44f14df06128bed816ca9993c
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2025-10-01 10:35:36 +0800

    feat: 设置页面

--- src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/ProgressRing.axaml.cs
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/ProgressRing.axaml.cs
@@ -6,4 +6,12 @@ namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls;
 
 public class ProgressRing : TemplatedControl
 {
+    public static readonly StyledProperty<int> RingWidthProperty =
+        AvaloniaProperty.Register<ProgressRing, int>(nameof(RingWidth), 5);
+
+    public int RingWidth
+    {
+        get => GetValue(RingWidthProperty);
+        set => SetValue(RingWidthProperty, value);
+    }
 }
