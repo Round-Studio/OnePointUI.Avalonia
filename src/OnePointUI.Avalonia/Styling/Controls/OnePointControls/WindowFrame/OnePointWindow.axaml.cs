bedb275f41646af28b17f22e8d2ad74bc9bb538b
Dime <liy_0924@qq.com>
2025-11-01 18:36:39 +0800

    fix: 在 Windows 中，无边框窗口最大化时的边距问题

--- src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/WindowFrame/OnePointWindow.axaml.cs
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/WindowFrame/OnePointWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 
 namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls.WindowFrame;
 
@@ -28,9 +29,23 @@ public partial class OnePointWindow : Window
     }
 
     private object? _mainContent { get; set; }
+    private Timer _stateTimer;
     public OnePointWindow()
     {
         InitializeComponent();
+
+        _stateTimer = new(state =>
+        {
+            Dispatcher.UIThread.Invoke(() =>
+            {
+                if (OperatingSystem.IsWindows())
+                {
+                    if (WindowState == WindowState.Maximized) this.Padding = new Thickness(8);
+                    else this.Padding = new Thickness(0);
+                }
+            });
+        });
+        _stateTimer.Change(TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(100));
     }
 
     private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
