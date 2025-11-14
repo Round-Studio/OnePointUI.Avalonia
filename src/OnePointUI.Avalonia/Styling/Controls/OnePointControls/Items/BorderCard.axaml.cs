ff7429857aa691c4333800ac19d37930d02d25aa
Dime <liy_0924@qq.com>
2025-11-15 00:46:38 +0800

    feat: BorderCard 控件

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/Items/BorderCard.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls.Items;
+
+public class BorderCard : ContentControl
+{
+}
