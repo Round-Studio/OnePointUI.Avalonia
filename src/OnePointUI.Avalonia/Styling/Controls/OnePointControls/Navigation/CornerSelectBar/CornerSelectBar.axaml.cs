1314a9f065f98f56f4aa48953b9497d3a686ac98
Dime <liy_0924@qq.com>
2025-11-29 00:55:16 +0800

    feat: 添加新的导航控件

---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/Navigation/CornerSelectBar/CornerSelectBar.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation.CornerSelectBar;
+
+public class CornerSelectBar : ListBox
+{
+}
---
+++ src/OnePointUI.Avalonia/Styling/Controls/OnePointControls/Navigation/CornerSelectBar/CornerSelectBarItem.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OnePointUI.Avalonia.Styling.Controls.OnePointControls.Navigation.CornerSelectBar;
+
+public class CornerSelectBarItem : ListBoxItem
+{
+    public static readonly StyledProperty<string> GlyphProperty =
+        AvaloniaProperty.Register<CornerSelectBarItem, string>(nameof(Glyph), "");
+    
+    
+    public static readonly StyledProperty<string> ItemTextProperty =
+        AvaloniaProperty.Register<CornerSelectBarItem, string>(nameof(ItemText), "Item");
+
+    public string Glyph
+    {
+        get => GetValue(GlyphProperty);
+        set => SetValue(GlyphProperty, value);
+    }
+
+    public string ItemText
+    {
+        get => GetValue(ItemTextProperty);
+        set => SetValue(ItemTextProperty, value);
+    }
+}
