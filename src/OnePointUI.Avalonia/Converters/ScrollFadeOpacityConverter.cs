382d467d0e2339559087ccd35bda9133646e9813
Dime <liy_0924@qq.com>
2026-08-13 02:20:52 +0800

    feat: 修改控件外观，为 ScrollViewer 加入上下两边的渐入效果

---
+++ src/OnePointUI.Avalonia/Converters/ScrollFadeOpacityConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace OnePointUI.Avalonia.Converters
+{
+    public class ScrollViewerOpacityMaskConverter : IMultiValueConverter
+    {
+        public double FadeSize { get; set; } = 30.0;
+
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values.Count >= 3 &&
+                values[0] is double offsetY &&
+                values[1] is double extentHeight &&
+                values[2] is double viewportHeight &&
+                viewportHeight > 0)
+            {
+                double maxScroll = extentHeight - viewportHeight;
+
+                double topRatio = Math.Min(FadeSize / viewportHeight, 0.5);
+                double bottomRatio = Math.Min(FadeSize / viewportHeight, 0.5);
+
+                double topAlphaFactor = maxScroll > 0 ? Math.Min(1.0, offsetY / FadeSize) : 0.0;
+
+                double remaining = maxScroll - offsetY;
+                double bottomAlphaFactor = maxScroll > 0 ? Math.Min(1.0, Math.Max(0.0, remaining / FadeSize)) : 0.0;
+
+                var brush = new LinearGradientBrush
+                {
+                    StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
+                    EndPoint = new RelativePoint(0, 1, RelativeUnit.Relative),
+                    GradientStops = new GradientStops
+                    {
+                        new GradientStop(Color.FromArgb((byte)(255 * (1 - topAlphaFactor)), 0, 0, 0), 0.0),
+                        new GradientStop(Colors.Black, topRatio),
+
+                        new GradientStop(Colors.Black, 1.0 - bottomRatio),
+
+                        new GradientStop(Color.FromArgb((byte)(255 * (1 - bottomAlphaFactor)), 0, 0, 0), 1.0)
+                    }
+                };
+
+                return brush;
+            }
+
+            return Brushes.Black;
+        }
+    }
+}
